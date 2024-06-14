b945c1ce483eac7a75c0807d79e9a97b4f016a05
CarlosMateoM <carlosmateo484@gmail.com>
2024-06-14 14:57:28 -0500

    add: enable email verification from any device

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 
 class VerifyEmailController extends Controller
@@ -13,12 +15,17 @@ class VerifyEmailController extends Controller
     /**
      * Mark the authenticated user's email address as verified.
      */
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
+    public function __invoke(User $user, EmailVerificationRequest $request): RedirectResponse|JsonResponse
     {
-        if ($request->user()->hasVerifiedEmail()) {
+
+        //$user = User::find($request->route('id'));
+
+        if ($user->hasVerifiedEmail()) {
             return redirect()->intended(
-                config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1'
+                config('app.frontend_url') . RouteServiceProvider::HOME . '?email_verified=1'
             );
+            
+            //return response()->json(['status' => 'already-verified']);
         }
 
         if ($request->user()->markEmailAsVerified()) {
@@ -26,7 +33,9 @@ class VerifyEmailController extends Controller
         }
 
         return redirect()->intended(
-            config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1'
+            config('app.frontend_url') . RouteServiceProvider::HOME . '?email_verified=1'
         );
+
+        //return response()->json(['status' => 'verified']);
     }
 }
